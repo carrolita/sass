46474c61ac292db05a31b1e4d3923f6de6247760
Caroline Granlund <granlundcaroline@gmail.com>
2018-10-14 18:08:28 +0200

    assignment completed-Caroline

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,13 +1,46 @@
 import React from "react"
-@import 'styles/_colors.scss';
-@import '~nprogress/nprogress'
+import "./App.scss"
 
 class App extends React.Component {
 
   render() {
     return (
-      <div>
-        Find me in src/app.js!
+      <div className="container">
+        <div className="centered-container">
+          <div className="tab-container">
+            <div className="tab active">
+              <span>Flyg</span>
+            </div>
+            <div className="tab">
+              <span>Flyg + Hotell</span>
+            </div>
+            <div className="tab">
+              <span>Hyrbil</span>
+            </div>
+            <div className="tab">
+              <span>Hotell</span>
+            </div>
+            <div className="tab">
+              <span>Reward</span>
+            </div>
+          </div>
+          <form className="form" action="/action_page.php">
+            <div className="input-items">
+              <div className="input-container">
+                <span htmlFor="from">Flyg från</span>
+                <input id="from" className="input" type="text" name="from" placeholder="Stockholm-Arlanda (ARN)" />
+              </div>
+              <div className="input-container">
+                <span htmlFor="to">Flyg till</span>
+                <input id="to" className="input" type="text" name="to" placeholder="Vart vill du resa?" />
+              </div>
+            </div>
+            <div className="button-container">
+              <button className="button" type="submit">Sök och beställ</button>
+            </div>
+          </form>
+        </div>
+
       </div>
     )
   }
